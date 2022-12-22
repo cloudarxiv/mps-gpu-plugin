a08db4c40f7b278f92422cefca795249392bd049
Michele Zanotti <m.zanotti019@gmail.com>
2022-12-22 18:25:14 +0100

    Remove constraint replicas <= 2

--- api/config/v1/replicas.go
+++ api/config/v1/replicas.go
@@ -206,10 +206,6 @@ func (s *ReplicatedResource) UnmarshalJSON(b []byte) error {
 		return err
 	}
 
-	if s.Replicas < 2 {
-		return fmt.Errorf("number of replicas must be >= 2")
-	}
-
 	rename, exists := rr["rename"]
 	if !exists {
 		return nil
